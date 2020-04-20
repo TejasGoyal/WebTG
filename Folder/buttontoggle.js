c12b2f55c7f5fc2bebf0e97acb4829161fa1453d
Tejas Goyal <53015200+TejasGoyal@users.noreply.github.com>
2020-04-20 17:10:38 +0530

    support files

---
+++ Folder/buttontoggle.js
@@ -0,0 +1,28 @@
+<!--Tejas Goyal-->
+	$(document).ready(function(){
+		$(".some-C").hide();
+	
+		$("#home-B").click(function(){
+			$(".some-C").hide();
+			$("#home-C").show();
+		});
+		$("#books-B").click(function(){
+			$(".some-C").hide();
+			$("#books-C").show();
+		});
+		$("#gallery-B").click(function(){
+			$(".some-C").hide();
+			$("#gallery-C").show();
+		});
+		$("#videos-B").click(function(){
+			$(".some-C").hide();
+			$("#videos-C").show();
+		});
+		$("#aboutme-B").click(function(){
+			$(".some-C").hide();
+			$("#aboutme-C").show();
+		});
+	});
+<!--Click on tab script-->
+
+
